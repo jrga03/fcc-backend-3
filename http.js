34829128c208ddc4f753ec7d1eb7d3541b9def24
Jason Acido <jasonacido@gmail.com>
2017-11-07 02:00:29 +0000

    cleaned code

--- http.js
+++ http.js
@@ -4,17 +4,6 @@ var url = process.argv[2]
 
 var print = http.get(url, function(response){
     response.setEncoding("utf8")
-    response.on("data", function(data){
-        console.log(data)
-    })
-    response.on("error", function(error){
-        console.log(error)
-    })
-    response.on("end", function(end){
-        // console.log(end)
-    })
-})
-
-// var print = http.get(url)
-
-// console.log(print)
+    response.on("data", (data) => { console.log(data) })
+    response.on("error", (error) => { console.log(error) })
+}).on('error', console.error)
